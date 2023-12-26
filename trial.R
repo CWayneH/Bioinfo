c5ee14a7769c98ca6aa88bdb017be3f412bba3ee
CWayneH <90815681+CWayneH@users.noreply.github.com>
2023-12-27 01:26:49 +0800

    Add files via upload

---
+++ trial.R
@@ -0,0 +1,58 @@
+df <- read.table('mouse/178/pca_cell_file.txt')
+lb <- read.table('mouse/178/label.txt')
+
+dat <- df
+dat$label <- lb
+
+# select subset of the data
+foldn <- 9
+set.seed(65)
+idx <- sample(foldn, nrow(dat), replace = TRUE)#, prob = foldn)
+#initial
+require(ranger)
+require(pROC)
+res_set <- c()
+res_train <- c()
+res_valid <- c()
+res_test <- c()
+res_auc <- c()
+for (i in 1:foldn) {
+  j <- i+1 #2,3,4,5,6
+  if(j>foldn){j <- j-foldn}
+  test <- dat[idx==i,]
+  valid <- dat[idx==j,]
+  train <- dat[idx!=i&idx!=j,]
+  #prediction in ranger
+  model <- ranger(label ~ .,
+                  data=train)
+  pred_train <- predict(model, train[,!(names(train)=="label")])
+  pred_test <- predict(model, test[,!(names(test)=="label")])
+  pred_valid <- predict(model, valid[,!(names(valid)=="label")])
+  
+  auc_train <- multiclass.roc(train$label$V1, pred_train[["predictions"]])
+  auc_test <- multiclass.roc(test$label$V1, pred_test[["predictions"]])
+  auc_valid <- multiclass.roc(valid$label$V1, pred_valid[["predictions"]])
+  res_train <- c(res_train, round(auc_train$auc,2))
+  res_test <- c(res_test, round(auc_test$auc,2))
+  res_valid <- c(res_valid, round(auc_valid$auc,2))
+  res_set <- c(res_set, paste0("fold",i))
+}
+
+
+out_data<-data.frame(set=res_set, training=res_train, 
+                     validation=res_valid, testing=res_test, 
+                     stringsAsFactors = F)
+out_data<-rbind(out_data,c('ave.', round(mean(res_train),2), 
+                           round(mean(res_valid),2), 
+                           round(mean(res_test),2)))
+print(out_data)
+
+#make testing performance
+pidx <- which.max(out_data$validation)
+ptrain <- dat[idx!=pidx,]
+ptest <- dat[idx==pidx,]
+model_prodc <- ranger(label ~ .,
+                      data=ptrain)
+pred_ptest <- predict(model_prodc, ptest[,!(names(ptest)=="label")])
+auc_ptest <- multiclass.roc(ptest$label$V1, pred_ptest[["predictions"]])
+print(auc_ptest)
