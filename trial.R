3afe9c0bbe74a0f4aacb9076ec0ab157050dbc37
CWayneH <90815681+CWayneH@users.noreply.github.com>
2024-02-25 23:59:31 +0800

    Update trial.R

--- trial.R
+++ trial.R
@@ -1,21 +1,24 @@
-metric.value <- function(query_m) {
+value.metric <- function(truth, pred) {
+  truth <- factor(truth, levels = c(0,1,2))
+  pred <- factor(pred, levels = c(0,1,2))
+  query_m <- table(truth, pred)
   # MCC, Acc, F1 and Precision
   acc <- sum(diag(query_m))/sum(query_m) # ACC
   pcs <- diag(query_m)/colSums(query_m) # precision for multi-class
   rcl <- diag(query_m)/rowSums(query_m) # recall for multi-class
-  ave.pcs <- mean(pcs) # Macro-averaging precision
+  ave.pcs <- mean(pcs, na.rm = TRUE) # Macro-averaging precision
   f1 <- 2*pcs*rcl/(pcs+rcl)
-  ave.f1 <- mean(f1) # Macro-averaging f1
+  ave.f1 <- mean(f1, na.rm = TRUE) # Macro-averaging f1
   # (TP*TN – FP*FN) / √(TP+FP)(TP+FN)(TN+FP)(TN+FN)
   tp <- diag(query_m)
   fn <- rowSums(query_m) - diag(query_m)
   fp <- colSums(query_m) - diag(query_m)
   tn <- sum(query_m) - tp - fn - fp
   mcc <- (tp*tn-fp*fn)/sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
-  ave.mcc <- mean(mcc) # Macro-averaging mcc
+  ave.mcc <- mean(mcc, na.rm = TRUE) # Macro-averaging mcc
   c(ave.mcc, acc, ave.f1, ave.pcs)
 }
-setwd("D:/../scHiCStackL")
+setwd("D:/Wayne/研究/scHiCStackL")
 df <- read.table('mouse/178/pca_cell_file.txt')
 lb <- read.table('mouse/178/label.txt')
 
@@ -42,27 +45,18 @@ for (i in 1:foldn) {
   train <- dat[idx!=i&idx!=j,]
   #prediction in ranger
   model <- ranger(label ~ .,
-                  data=train)
+                  data=train, classification = TRUE)
   pred_train <- predict(model, train[,!(names(train)=="label")])
   pred_test <- predict(model, test[,!(names(test)=="label")])
   pred_valid <- predict(model, valid[,!(names(valid)=="label")])
   
-  #handle confusion matrix and logLikelihood
-  cm <- c(cm.make(query_m, d$reference, d$pred.score, thrsd))
-  tp_d <- cm[1]
-  tn_d <- cm[2]
-  fn_d <- cm[3]
-  fp_d <- cm[4]
-  snsty <- tp_d/(tp_d+fn_d)
-  spcty <- tn_d/(tn_d+fp_d)
-  pcsn <- tp_d/(tp_d+fp_d)
-  f1 <- (2*snsty*pcsn)/(snsty+pcsn)
-  lglkhd <- cm[5]
-  nlglkhd <- sum(log(ifelse(d$reference == query_m, (tp_d+fn_d)/dim(d)[[1]], (tn_d+fp_d)/dim(d)[[1]])))
-  psudR2 <- 1-(-2*lglkhd)/(-2*nlglkhd)
-  cm_train <- table(truth=train$label$V1, pred=pred_train[["predictions"]])
-  cm_test <- table(truth=test$label$V1, pred=pred_test[["predictions"]])
-  cm_valid <- table(truth=valid$label$V1, pred=pred_valid[["predictions"]])
+  res.train <- value.metric(train$label$V1, pred_train[["predictions"]])
+  res.test <- value.metric(test$label$V1, pred_test[["predictions"]])
+  res.valid <- value.metric(valid$label$V1, pred_valid[["predictions"]])
+  
+  # cm_train <- table(truth=train$label$V1, pred=pred_train[["predictions"]])
+  # cm_test <- table(truth=test$label$V1, pred=pred_test[["predictions"]])
+  # cm_valid <- table(truth=valid$label$V1, pred=pred_valid[["predictions"]])
   
   
   auc_train <- multiclass.roc(train$label$V1, pred_train[["predictions"]])
