591b7365f4c12374004a39957d22fba0072ab969
CWayneH <90815681+CWayneH@users.noreply.github.com>
2024-02-27 00:19:21 +0800

    Update trial.R

--- trial.R
+++ trial.R
@@ -1,4 +1,4 @@
-value.metric <- function(truth, pred) {
+value.metric <- function(truth, pred, m) {
   truth <- factor(truth, levels = c(0,1,2))
   pred <- factor(pred, levels = c(0,1,2))
   query_m <- table(truth, pred)
@@ -17,10 +17,27 @@ value.metric <- function(truth, pred) {
   mcc <- (tp*tn-fp*fn)/sqrt((tp+fp)*(tp+fn)*(tn+fp)*(tn+fn))
   ave.mcc <- mean(mcc, na.rm = TRUE) # Macro-averaging mcc
   res <- c(ave.mcc, acc, ave.f1, ave.pcs)
-  names(res) <- c('MCC', 'Acc', 'F1', 'Precision')
+  names(res) <- paste0(m, c('.MCC', '.Acc', '.F1', '.Precision'))
   return(res)
 }
-setwd("C:/Users/Wayne-NB/Desktop/MASTER.InService/研究")
+model.ctrl <- function(m, t, d) {
+  switch (m,
+    ranger = {
+      model <- ranger(label ~ .,
+                      data=t, classification = TRUE)
+      pred <- predict(model, d[,!(names(d)=="label")])[["predictions"]]
+    },
+    rpart = {
+      model <- rpart(t$label$V1 ~ .,
+                     data=t, control=rpart.control(minsplit = 3, minbucket = 2, maxcompete = 3, maxdepth = 11),
+                     method="class")
+      pred <- predict(model, newdata = d, type="class")
+    }
+  )
+  truth <- d$label$V1
+  return(value.metric(truth, pred, m))
+}
+setwd("D:/../scHiCStackL")
 df <- read.table('mouse/178/pca_cell_file.txt')
 lb <- read.table('mouse/178/label.txt')
 
@@ -28,11 +45,12 @@ dat <- df
 dat$label <- lb
 
 # select subset of the data
-foldn <- 6
+foldn <- 4
 set.seed(65)
 idx <- sample(foldn, nrow(dat), replace = TRUE)#, prob = foldn)
 #initial
 require(ranger)
+require(rpart)
 # require(pROC)
 # res_set <- c()
 # res_train <- c()
@@ -45,17 +63,27 @@ for (i in 1:foldn) {
   test <- dat[idx==i,]
   valid <- dat[idx==j,]
   train <- dat[idx!=i&idx!=j,]
-  #prediction in ranger
-  model <- ranger(label ~ .,
-                  data=train, classification = TRUE)
-  pred_train <- predict(model, train[,!(names(train)=="label")])
-  pred_test <- predict(model, test[,!(names(test)=="label")])
-  pred_valid <- predict(model, valid[,!(names(valid)=="label")])
+  # #prediction in ranger
+  # model <- ranger(label ~ .,
+  #                 data=train, classification = TRUE)
+  # #predict in dt
+  # model <- rpart(train$label$V1 ~ .,
+  #                data=train, control=rpart.control(minsplit = 3, minbucket = 2, maxcompete = 3, maxdepth = 11),
+  #                method="class")
+  # pred_train <- predict(model, train[,!(names(train)=="label")])
+  # pred_test <- predict(model, test[,!(names(test)=="label")])
+  # pred_valid <- predict(model, valid[,!(names(valid)=="label")])
   
-  res.train <- value.metric(train$label$V1, pred_train[["predictions"]])
-  res.test <- value.metric(test$label$V1, pred_test[["predictions"]])
-  res.valid <- value.metric(valid$label$V1, pred_valid[["predictions"]])
+  # res.train <- value.metric(train$label$V1, pred_train[["predictions"]])
+  # res.test <- value.metric(test$label$V1, pred_test[["predictions"]])
+  # res.valid <- value.metric(valid$label$V1, pred_valid[["predictions"]])
   
+  res.train.ranger <- model.ctrl("ranger", train, train)
+  res.test.ranger <- model.ctrl("ranger", train, test)
+  res.valid.ranger <- model.ctrl("ranger", train, valid)
+  res.train.rpart <- model.ctrl("rpart", train, train)
+  res.test.rpart <- model.ctrl("rpart", train, test)
+  res.valid.rpart <- model.ctrl("rpart", train, valid)
   # cm_train <- table(truth=train$label$V1, pred=pred_train[["predictions"]])
   # cm_test <- table(truth=test$label$V1, pred=pred_test[["predictions"]])
   # cm_valid <- table(truth=valid$label$V1, pred=pred_valid[["predictions"]])
@@ -67,7 +95,9 @@ for (i in 1:foldn) {
   # res_train <- c(res_train, round(res.train,2))
   # res_test <- c(res_test, round(res.test,2))
   # res_valid <- c(res_valid, round(res.valid,2))
-  res.set <- rbind(res.train, res.test, res.valid)
+  res.set.ranger <- rbind(res.train.ranger, res.test.ranger, res.valid.ranger)
+  res.set.raprt <- rbind(res.train.rpart, res.test.rpart, res.valid.rpart)
+  res.set <- cbind(res.set.ranger, res.set.raprt)
   set.name <- paste0("fold.", i, c("_train","_test","_valid"))
   rownames(res.set) <- set.name
   res.set.mg <- rbind(res.set.mg, res.set)
